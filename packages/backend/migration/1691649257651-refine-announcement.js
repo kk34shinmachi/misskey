b0d7e7a14d60c0412505318c8b83d80d169e80d5
JJ0UYX <128118344+yurihisada@users.noreply.github.com>
2023-11-19 10:03:16 +0900

    マイグレーションに失敗する問題の修正

--- packages/backend/migration/1691649257651-refine-announcement.js
+++ packages/backend/migration/1691649257651-refine-announcement.js
@@ -6,10 +6,8 @@ export class RefineAnnouncement1691649257651 {
         await queryRunner.query(`ALTER TABLE "announcement" ADD "needConfirmationToRead" boolean NOT NULL DEFAULT false`);
         await queryRunner.query(`ALTER TABLE "announcement" ADD "isActive" boolean NOT NULL DEFAULT true`);
         await queryRunner.query(`ALTER TABLE "announcement" ADD "forExistingUsers" boolean NOT NULL DEFAULT false`);
-        await queryRunner.query(`ALTER TABLE "announcement" ADD "userId" character varying(32)`);
         await queryRunner.query(`CREATE INDEX "IDX_bc1afcc8ef7e9400cdc3c0a87e" ON "announcement" ("isActive") `);
         await queryRunner.query(`CREATE INDEX "IDX_da795d3a83187e8832005ba19d" ON "announcement" ("forExistingUsers") `);
-        await queryRunner.query(`CREATE INDEX "IDX_fd25dfe3da37df1715f11ba6ec" ON "announcement" ("userId") `);
         await queryRunner.query(`ALTER TABLE "announcement" ADD CONSTRAINT "FK_fd25dfe3da37df1715f11ba6ec8" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
